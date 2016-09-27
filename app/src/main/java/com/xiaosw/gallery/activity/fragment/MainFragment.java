b348e6647329adcbeaf338b7f5fad37c7fd693da
xiaosw <xiaosw@putao.com>
2016-09-27 10:35:44 +0800

    处理首页fragment数据重复初始化

--- app/src/main/java/com/xiaosw/gallery/activity/fragment/MainFragment.java
+++ app/src/main/java/com/xiaosw/gallery/activity/fragment/MainFragment.java
@@ -1,5 +1,6 @@
 package com.xiaosw.gallery.activity.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -52,6 +53,13 @@ public class MainFragment extends BaseFragment implements DateLineTabFragment.On
     @Bind(R.id.view_header_container)
     View view_functoin_header_container; // 功能操作头部
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mPages = new ArrayList<>();
+        initPages();
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -72,8 +80,6 @@ public class MainFragment extends BaseFragment implements DateLineTabFragment.On
     }
 
     private void initViewPager() {
-        mPages = new ArrayList<>();
-        initPages();
         mMainPagerAdapter = new MainPagerAdapter(getChildFragmentManager(), mPages);
         mMainViewPager.setAdapter(mMainPagerAdapter);
         mMainViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
